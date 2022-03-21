a8b5391ab53a4017debbecff7a66cf3df7390ac4
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-21 21:00:08 +0500

    Routing Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import ProfileCard from "./Components/ProfileCard";
 import profile from "./Api/profile";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <section className="container">
-        <ProfileCard profile={profile} />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<ProfileCard profile={profile} />} />
+            <Route path="/form" />
+          </Routes>
+        </BrowserRouter>
       </section>
     </div>
   );
