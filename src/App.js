2aab5ca327a9a2750d2005d377ba6793265e2d7f
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-21 14:30:31 +0500

    Basic Styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
+import ProfileCard from "./ProfileCard";
 
 function App() {
-  return <div className="App">Profile Card</div>;
+  return (
+    <div className="App">
+      <section className="container">
+        <ProfileCard />
+      </section>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/ProfileCard/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+const ProfileCard = () => {
+  return (
+    <div className="cardContainer">
+      <section className="profileCard">Profile Card</section>
+    </div>
+  );
+};
+
+export default ProfileCard;
