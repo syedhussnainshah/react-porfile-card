74c3f645ed7c5708390ad89a6c239f0eaedf2db8
MTahir27 <m.tahiridrees27@gmail.com>
2022-03-21 22:36:45 +0500

    Edit Form

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -13,41 +13,62 @@ const Form = () => {
   };
   return (
     <form>
-      <h1>Personal Info</h1>
-      <InputField label="Full name" type="text" id="name" name="name" />
-      <InputField
-        label="profile image"
-        type="file"
-        id="profileimg"
-        name="profileImg"
-      />
-      <InputField label="email" type="text" id="email" name="email" />
-      <InputField
-        label="profession"
-        type="text"
-        id="profession"
-        name="profession"
-      />
-      <InputField
-        label="website Link"
-        type="text"
-        id="website"
-        name="website"
-      />
-      <h1>Social Links</h1>
-      <InputField label="linkedin" type="text" id="linkedin" name="linkedin" />
-      <InputField label="twitter" type="text" id="twitter" name="twitter" />
-      <InputField label="facebook" type="text" id="facebook" name="facebook" />
-      <InputField
-        label="instagram"
-        type="text"
-        id="instagram"
-        name="instagram"
-      />
-      <InputField label="github" type="text" id="github" name="github" />
-      <h1>Description</h1>
-      <TextAreaField label="about" type="text" id="about" name="about" />
-      <TextAreaField label="intrest" type="text" id="intrest" name="intrest" />
+      <section>
+        <h1>Personal Info</h1>
+        <InputField label="Full name" type="text" id="name" name="name" />
+        <InputField
+          label="profile image"
+          type="file"
+          id="profileimg"
+          name="profileImg"
+        />
+        <InputField label="email" type="text" id="email" name="email" />
+        <InputField
+          label="profession"
+          type="text"
+          id="profession"
+          name="profession"
+        />
+        <InputField
+          label="website Link"
+          type="text"
+          id="website"
+          name="website"
+        />
+      </section>
+      <section>
+        <h1>Description</h1>
+        <TextAreaField label="about" type="text" id="about" name="about" />
+        <TextAreaField
+          label="intrest"
+          type="text"
+          id="intrest"
+          name="intrest"
+        />
+      </section>
+      <section>
+        <h1>Social Links</h1>
+        <InputField
+          label="linkedin"
+          type="text"
+          id="linkedin"
+          name="linkedin"
+        />
+        <InputField label="twitter" type="text" id="twitter" name="twitter" />
+        <InputField
+          label="facebook"
+          type="text"
+          id="facebook"
+          name="facebook"
+        />
+        <InputField
+          label="instagram"
+          type="text"
+          id="instagram"
+          name="instagram"
+        />
+        <InputField label="github" type="text" id="github" name="github" />
+      </section>
       <div>
         <CustomButton title="Submit Form" />
       </div>
